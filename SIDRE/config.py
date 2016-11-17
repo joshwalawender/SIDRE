ff8849e8df0eb27d0bca6003ba0153185dd3f330
Josh Walawender <jmwalawender@gmail.com>
2016-11-16 22:56:06 -1000

    Update config with new default file location and remove unused function

--- SIDRE/config.py
+++ SIDRE/config.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 
-def get_config(config_file='~/.SIDRE.yaml'):
+def get_config(config_file='SIDRE.yaml'):
     config_file = os.path.expanduser(config_file)
     if not os.path.exists(config_file):
         config = {'Default': True}
@@ -10,19 +10,6 @@ def get_config(config_file='~/.SIDRE.yaml'):
             contents = FO.read()
             config = yaml.load(contents)
     return config
-
-
-def get_file_config(datapath, filename='calibration_files.yaml'):
-    assert os.path.exists(datapath)
-    config_file = os.path.join(datapath, filename)
-    if not os.path.exists(config_file):
-        config = {'Default': True}
-    else:
-        with open(config_file, 'r') as FO:
-            contents = FO.read()
-            config = yaml.load(contents)
-    return config
-
 
 
 if __name__ == '__main__':
