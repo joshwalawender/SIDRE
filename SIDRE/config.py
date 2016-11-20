59fac937ebee02f3e9c03417f3b467227eb33527
Josh Walawender <jmwalawender@gmail.com>
2016-11-20 09:24:19 -1000

    Have config expand paths

--- SIDRE/config.py
+++ SIDRE/config.py
@@ -9,6 +9,15 @@ def get_config(config_file='SIDRE.yaml'):
         with open(config_file, 'r') as FO:
             contents = FO.read()
             config = yaml.load(contents)
+
+    # Expand paths
+    for pathname in ['BiasPath', 'DarkPath', 'FlatPath', 'MasterPath']:
+        try:
+            config[pathname] = os.path.expanduser(config[pathname])
+            config[pathname] = os.path.abspath(config[pathname])
+        except:
+            pass
+
     return config
 
 
