76585519de57dbef7b18eae3bbfbcefea78dbd58
Josh Walawender <jmwalawender@gmail.com>
2016-11-23 19:48:05 -1000

    Add methods to solve astrometry and read header for pointing

--- SIDRE/core.py
+++ SIDRE/core.py
@@ -1,12 +1,19 @@
 import os
 import sys
 import re
+import tempfile
 import logging
 from glob import glob
 from datetime import datetime as dt
+if sys.version_info.major == 2:
+    import subprocess32 as subprocess
+elif sys.version_info.major == 3:
+    import subprocess
 
 from astropy.io import fits
+from astropy import wcs
 import astropy.units as u
+import astropy.coordinates as c
 import ccdproc
 
 from .config import get_config
@@ -40,6 +47,7 @@ class ScienceImage(object):
         assert os.path.exists(file)
         self.file = file
         self.filename = os.path.basename(file)
+        self.fileroot = os.path.splitext(self.filename)[0]
         if preprocess_header:
             preprocess_header
         try:
@@ -55,7 +63,7 @@ class ScienceImage(object):
         self.add_logger()
         self.log.info('Processing File: {}'.format(self.filename))
 
-
+        self.header_pointing = None
 
 
     def add_logger(self, logfile=None, verbose=False):
@@ -115,7 +123,7 @@ class ScienceImage(object):
         # Bias correct the image
         master_bias = get_master(self.date, type='Bias')
         if master_bias:
-            print('Subtracting master bias')
+            self.log.info('Subtracting master bias')
             self.ccd = ccdproc.subtract_bias(self.ccd, master_bias, add_keyword=None)
             metadata.set('BIASFILE', value=master_bias.header['FILENAME'],
                      comment='Filename of master bias file')
@@ -131,7 +139,7 @@ class ScienceImage(object):
         except KeyError:
             gain.value = self.config.get('Gain')
 
-        print('Gain correcting image')
+        self.log.info('Gain correcting image')
         self.ccd = ccdproc.gain_correct(self.ccd, gain.value)
 
         # Dark correct the image
@@ -140,7 +148,7 @@ class ScienceImage(object):
         exptime.value_from(self.ccd.header)
         master_dark = get_master(self.date, type='Dark')
         if master_dark:
-            print('Dark correcting image')
+            self.log.info('Dark correcting image')
             self.ccd = ccdproc.subtract_dark(self.ccd, master_dark,
                            data_exposure=exptime.value,
                            dark_exposure=1.0*u.second,
@@ -158,19 +166,19 @@ class ScienceImage(object):
 #             readnoise.value_from(self.ccd.header)
 #         except KeyError:
 #             readnoise.value = self.config.get('RN')
-#         print('Estimating uncertainty')
+#         self.log.info('Estimating uncertainty')
 #         self.ccd = ccdproc.create_deviation(self.ccd, readnoise=readnoise.value)
 
         # Shutter correct the image
         shutter_map = get_master_shutter_map(self.date)
         if shutter_map:
-            print('Applying shutter map')
+            self.log.info('Applying shutter map')
             self.ccd = ccdproc.apply_shutter_map(self.ccd, shutter_map)
 
         # Flat correct the image
         master_flat = get_master(self.date, type='Flat')
         if master_flat:
-            print('Flat fielding image')
+            self.log.info('Flat fielding image')
             self.ccd = ccdproc.flat_correct(im, master_flat, add_keyword=None)
             metadata.set('FLATFILE', value=master_flat.header['FILENAME'],
                      comment='Filename of master flat file')
@@ -186,3 +194,77 @@ class ScienceImage(object):
         for key in metadata.keys():
             print(key, metadata[key])
 
+    def get_header_pointing(self):
+        '''
+        Read the pointing coordinate from the header RA and DEC keywords.
+        '''
+        RAkwd = self.config.get('RA', 'RA')
+        DECkwd = self.config.get('DEC', 'DEC')
+        coord_frame = self.config.get('CoordinateFrame', 'Fk5')
+        coord_format = self.config.get('CoordinateFormat', 'HMSDMS')
+        RA = self.ccd.header[RAkwd]
+        DEC = self.ccd.header[DECkwd]
+
+        if coord_format == 'HMSDMS':
+            self.header_pointing = c.SkyCoord(RA, DEC, frame=coord_frame,
+                                     unit=(u.hourangle, u.deg))
+        elif coord_format == 'decimal degrees':
+            self.header_pointing = c.SkyCoord(float(RA), float(DEC),
+                                     frame=coord_frame,
+                                     unit=(u.deg, u.deg))
+        elif coord_format == 'decimal hours degrees':
+            self.header_pointing = c.SkyCoord(float(RA), float(DEC),
+                                     frame=coord_frame,
+                                     unit=(u.hourangle, u.deg))
+        else:
+            self.header_pointing = None
+            self.log.warning('CoordinateFormat not understood.')
+
+
+    def render_jpeg(self, jpegfilename=None):
+        '''
+        '''
+        if not jpegfilename:
+            jpegfilename = '{}.jpg'.format(os.path.basename(self.filename))
+        
+        
+    def solve_astrometry(self, downsample=4, SIPorder=2):
+        '''
+        
+        '''
+        solvefield_args = self.config.get('SolveFieldArgs', [])
+        solvefield_args.extend(['-z', '{:d}'.format(downsample)])
+        solvefield_args.extend(['-t', '{:d}'.format(SIPorder)])
+        
+        # Create temporary directory
+        tdir = tempfile.mkdtemp()
+        tfile = os.path.join(tdir, self.filename)
+        ccdproc.fits_ccddata_writer(self.ccd, tfile)
+        
+        # run astrometry.net on the temporary fits file
+        cmd = ['solve-field', '-p']
+        cmd.extend(solvefield_args)
+        if not self.header_pointing:
+            self.get_header_pointing()
+        if self.header_pointing:
+            cmd.extend(['-3', '{:.4f}'.format(self.header_pointing.ra.degree)])
+            cmd.extend(['-4', '{:.4f}'.format(self.header_pointing.dec.degree)])
+        cmd.append(tfile)
+        fail = subprocess.call(cmd)
+        solved_file = os.path.join(tdir, '{}.solved'.format(self.fileroot))
+        wcs_file = os.path.join(tdir, '{}.wcs'.format(self.fileroot))
+        if not fail and os.path.exists(solved_file) and os.path.exists(wcs_file):
+            new_wcs = wcs.WCS(wcs_file)
+            if new_wcs.is_celestial:
+                self.ccd.wcs = new_wcs
+                self.ccd.header.add_history('New WCS solved by astrometry.net')
+                new_header = new_wcs.to_header(relax=True)
+                for key in new_header.keys():
+                    self.ccd.header.set(key, new_header[key], new_header.comments[key])
+        # Cleanup temporary directory
+        tfiles = glob(os.path.join(tdir, '*'))
+        for tf in tfiles:
+            os.remove(tf)
+        os.rmdir(tdir)
+        
+        
